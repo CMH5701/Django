c47474b02b5a847d8266cc0e9b255b3686715715
CMH5701 <skdaks5701@naver.com>
2022-06-28 19:30:23 +0900

    실습

--- Django/djproject/settings.py
+++ Django/djproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+import os , json
+from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -22,6 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-^+n44k3y3&o-d21b%bssmnvp79pvd)&nd5jyerzpvcm#-&r2i+'
 
+secret_file = os.path.join(BASE_DIR , 'secrets.json')
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+def get_secret(setting, secrets = secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set th {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
