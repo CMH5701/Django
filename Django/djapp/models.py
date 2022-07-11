6884e5669388fa2259dc71423f796e4c033188b4
CMH5701 <skdaks5701@naver.com>
2022-07-11 21:55:10 +0900

    CRUD페이지

--- Django/djapp/models.py
+++ Django/djapp/models.py
@@ -6,6 +6,6 @@ class Blog(models.Model):
     title = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('data published')
     body = models.TextField()
-    image = models.ImageField(upload_to = 'images/', blank =True)
+    image = models.ImageField(upload_to = 'images/', blank =True ,null = True)
     def __str__(self):
         return self.title
